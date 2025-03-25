dce3f6bc1aae80c897961bfb3c2eb61f12d68b30
Clinton Otse <otseclinton60@gmail.com>
2025-03-25 08:04:32 +0100

    Nav fix

--- Resellio/app/index.tsx
+++ Resellio/app/index.tsx
@@ -1,6 +1,8 @@
 import { View, Text, ImageBackground, Pressable } from "react-native";
 import { Link } from "expo-router";
+import { BlurView } from "expo-blur";
 
+import Icon from "react-native-vector-icons/Ionicons";
 import greyBG from "@/assets/images/grey-bg.png";
 
 const HomeScreen = () => {
@@ -10,19 +12,37 @@ const HomeScreen = () => {
       resizeMode="cover"
       className="dark:bg-black bg-white flex-1 pt-12 pb-5 px-5"
     >
-      <View className="flex flex-1">
-        <Text className="dark:text-white text-gray-800 text-[35px]">Resellio</Text>
+      <View className="relative flex flex-1">
+        <Text className="dark:text-white text-gray-800 text-[35px]">
+          Resellio
+        </Text>
 
-        <Link href="/menu" asChild>
-          <Pressable className="bg-black w-[100px] rounded-md p-3 mt-5 flex justify-center items-center">
-            <Text className="text-white">Menu</Text>
-          </Pressable>
-        </Link>
-        <Link href="/contact" asChild>
-          <Pressable className="bg-black w-[100px] rounded-md p-3 mt-5 flex justify-center items-center">
-            <Text className="text-white">Contact Us</Text>
-          </Pressable>
-        </Link>
+        <View className="absolute flex bottom-0 w-full rounded-[20px] overflow-hidden">
+          <BlurView
+            intensity={80}
+            tint="dark"
+            className="flex flex-row justify-evenly items-center p-5"
+          >
+            <Link href="/menu" asChild>
+              <Pressable>
+                <Icon
+                  name="menu-outline"
+                  size={24}
+                  className="mr-5 dark:!text-text-400 !text-white"
+                />
+              </Pressable>
+            </Link>
+            <Link href="/contact" asChild>
+              <Pressable>
+                <Icon
+                  name="mail-outline"
+                  size={24}
+                  className="mr-5 dark:!text-text-400 !text-white"
+                />
+              </Pressable>
+            </Link>
+          </BlurView>
+        </View>
       </View>
     </ImageBackground>
   );
