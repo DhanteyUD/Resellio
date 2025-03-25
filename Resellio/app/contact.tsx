52009a6307ccefcd2385ae07aab9834c54954931
Clinton Otse <otseclinton60@gmail.com>
2025-03-25 06:30:04 +0100

    Contact Us Page Fix

--- Resellio/app/contact.tsx
+++ Resellio/app/contact.tsx
@@ -1,35 +1,112 @@
-import { View, Text, Image } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
-
+import { View, Text, Image, TouchableOpacity, Linking } from "react-native";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import contactUsImage from "@/assets/images/contact-us.webp";
+import Icon from "react-native-vector-icons/Ionicons";
 
-export default function TabTwoScreen() {
+import { companyInfo } from "@/constants/CompanyInfo";
+
+export default function Contact() {
+  const handleLocationPress = (address1: string, address2: string): void => {
+    const address = encodeURIComponent(`${address1}, ${address2}`);
+    const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${address}`;
+    Linking.openURL(googleMapsUrl);
+  };
+
+  const handlePhonePress = (phone: string): void => {
+    Linking.openURL(`tel:${phone}`);
+  };
+
+  const handleMessagePress = (message: string): void => {
+    Linking.openURL(`sms:${message}`);
+  };
+
+  const handleMailPress = (mail: string): void => {
+    Linking.openURL(`mailto:${mail}`);
+  };
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
       headerImage={<Image source={contactUsImage} className="w-full h-full" />}
     >
-      <View>
-        <Text className="dark:text-light text-dark text-[20px] font-[700] mb-5">
-          Resellio
-        </Text>
-        <Text className="dark:text-light text-dark mb-1">555 Apple Lane</Text>
-        <Text className="dark:text-light text-dark mb-10">
-          Silicon Valley, CA 55555
-        </Text>
+      <View className="p-4">
+        <View className="flex-row items-center mb-10">
+          <Icon
+            name="business-outline"
+            size={24}
+            color="#4B5563"
+            className="mr-5"
+          />
+          <Text className="dark:text-light text-dark text-[20px] font-[700]">
+            {companyInfo.name}
+          </Text>
+        </View>
 
-        <Text className="dark:text-light text-dark text-[20px] font-[700] mb-5">
-          Phone
-        </Text>
-        <Text className="dark:text-light text-dark mb-10">(555) 555-5555</Text>
+        <TouchableOpacity
+          onPress={() =>
+            handleLocationPress(
+              companyInfo.addressLine1,
+              companyInfo.addressLine2
+            )
+          }
+          className="flex-row items-center mb-10"
+        >
+          <Icon
+            name="location-outline"
+            size={24}
+            color="#4B5563"
+            className="mr-5"
+          />
+          <View>
+            <Text className="dark:text-light text-dark">
+              {companyInfo.addressLine1}
+            </Text>
+            <Text className="dark:text-light text-dark">
+              {companyInfo.addressLine2}
+            </Text>
+          </View>
+        </TouchableOpacity>
 
-        <Text className="dark:text-light text-dark text-[20px] font-[700] mb-5">
-          Email
-        </Text>
-        <Text className="dark:text-light text-dark mb-10">
-          resellio@gmail.com
-        </Text>
+        <TouchableOpacity
+          onPress={() => handlePhonePress(companyInfo.phone)}
+          className="flex-row items-center mb-10"
+        >
+          <Icon
+            name="call-outline"
+            size={24}
+            color="#4B5563"
+            className="mr-5"
+          />
+          <Text className="dark:text-light text-dark">{companyInfo.phone}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => handleMessagePress(companyInfo.message)}
+          className="flex-row items-center mb-10"
+        >
+          <Icon
+            name="chatbubble-outline"
+            size={24}
+            color="#4B5563"
+            className="mr-5"
+          />
+          <Text className="dark:text-light text-dark">
+            {companyInfo.message}
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => handleMailPress(companyInfo.email)}
+          className="flex-row items-center mb-10"
+        >
+          <Icon
+            name="mail-outline"
+            size={24}
+            color="#4B5563"
+            className="mr-5"
+          />
+          <Text className="dark:text-light text-dark">{companyInfo.email}</Text>
+        </TouchableOpacity>
       </View>
     </ParallaxScrollView>
   );
